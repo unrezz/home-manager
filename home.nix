b51b883df4d39beeda74ff1aea06629252ff4406
unrezz <mikeypixel87@gmail.com>
2026-04-05 15:26:26 -0600

    first commit
    Slightly modified flake.nix
    added some things on the home.nix file

---
+++ home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should manage.
+  home.username = "unrezz";
+  home.homeDirectory = "/home/unrezz";
+
+  programs.git = {
+    enable = true;
+    userName = "unrezz";
+    userEmail = ""
+      aliases = {
+        pu = "push";
+        co = "checkout";
+        cm = "commit";
+      };
+  };
+
+  gtk = {
+    enable = true;
+    theme.name = "adw-gtk3";
+    cursorTheme.name = "Bibata-Modern-Ice";
+    iconTheme.name = "GrubvoxPlus"
+  };
+
+  xdf.mimeApps.defaultApplications = {
+    "text/plain" = [ "neovide.desktop" ];
+    "application/pdf" = [ "zathura.desktop" ];
+    "image/*" = [ "ristretto.desktop" ];
+    "video/png" = [ "mpv.desktop" ];
+    "video/jpg" = [ "mpv.desktop" ];
+    "video/*" = [ "mpv.desktop" ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    # enableAutosuggestions = true;
+    # enableCompletition = true;
+    # envExtra = ''
+    #   export SOMEZSHVARIABLE="something"
+    # '';
+  }
+
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  home.file = { # The primary way to manage plain dot files is through 'home.file'.
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either:
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  # or
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  # or
+  #  /etc/profiles/per-user/unrezz/etc/profile.d/hm-session-vars.sh
+
+  home.sessionVariables = {
+    EDITOR = "emacs";
+  };
+
+  home.stateVersion = "25.11"; # Avoid unnecessary changing.
+
+  programs.home-manager.enable = true; # Let Home Manager install and manage itself.
+}
